367baea2ab2a9d44d5e1a54db6b1797b0278b78b
KhawajaShaikh0610 <shaikhkhawaja0610@gmail.com>
2026-06-15 23:21:07 +0530

    config name

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
     dialect: process.env.DB_DIALECT,
+    port: process.env.MYSQLPORT,
     logging: false, // Set to true if you want to see SQL queries in console
     pool: {
       max: 5,
