53d966fcade52836db021a2f435cad551f177f27
KhawajaShaikh0610 <shaikhkhawaja0610@gmail.com>
2026-06-18 22:18:08 +0530

    env

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: "mysql",
     port: process.env.DB_PORT,
     logging: false, // Set to true if you want to see SQL queries in console
     pool: {
