4a8ff8f5b7530cce20ffef8175c826a75bf4a85f
asadshaikh41 <87890728+asadshaikh41@users.noreply.github.com>
2026-07-17 22:38:27 +0530

    code updated

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const startServer = async () => {
     console.log("Database connected");
 
     // Temporarily disable this
-    // await sequelize.sync();
+    await sequelize.sync();
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
